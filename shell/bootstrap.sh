233b021d1dd68a906459ee9917b02a656c2d60c8
Savio Lucena <saviogl@gmail.com>
2022-05-21 19:51:25 -0700

    feat: add gopath in path

--- shell/bootstrap.sh
+++ shell/bootstrap.sh
@@ -2,6 +2,7 @@ path_prepend "$HOME/.local/bin"
 path_prepend "$HOME/.dotfiles/bin"
 path_prepend "$HOME/.vim/plugged/fzf/bin"
 path_prepend "$HOME/.krew/bin"
+path_prepend "$HOME/go/bin"
 path_prepend "/opt/homebrew/opt/libpq/bin"
 
 export NVM_DIR="$HOME/.nvm"
